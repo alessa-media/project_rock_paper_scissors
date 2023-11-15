f112c91df289aa6900b7f05583424ab1a89c26d7
Alessa <139245506+alessa-media@users.noreply.github.com>
2023-11-15 16:15:48 +0100

    player names

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
 "use strict";
+let Spielername1 = prompt("Gib Spieler 1 einen Namen.")
+let Spielername2 = prompt("Gib Spieler 2 einen Namen.")
+
+document.getElementById("player1").innerText=Spielername1+" wählt...";
+document.getElementById("player2").innerText=Spielername2+" wählt...";
+
+function start_game(){
+    document.getElementById("game").style="display:block;"
+}
+
+
+
+let point
+
