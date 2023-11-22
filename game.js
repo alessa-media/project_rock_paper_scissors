f906d0d925806201bddfeb36f4db23ad1af03e4d
Miriam-MM <151748134+Miriam-MM@users.noreply.github.com>
2023-11-22 16:57:29 +0100

    Spiel funktioniert :)

--- game.js
+++ game.js
@@ -8,32 +8,42 @@ let Spieler2_Punkte
 let Auswahl_Spieler1 
 let Auswahl_Spieler2 
 let winner
+let Spielername1 
+let Spielername2 
 
 
 
-//Pop-up-Fenster, um beiden Spielern einen Namen zu geben.
-let Spielername1 = prompt("Gib Spieler 1 einen Namen.")
-let Spielername2 = prompt("Gib Spieler 2 einen Namen.")
 
-//damit der eingegebene Spielername auf der Seite angezeigt wird.
-document.getElementById("player1").innerText=Spielername1+" wählt...";
-document.getElementById("player2").innerText=Spielername2+" wählt...";
-
+//Spiel wird gestartet
 //Beide Spielblöcke werden angezeigt, sobald auf "Spiel starten" geklickt wird.
 function start_game(){
     document.getElementById("game").style="display:block;";
+    //Pop-up-Fenster, um beiden Spielern einen Namen zu geben.
+    Spielername1 = prompt("Gib Spieler 1 einen Namen.")
+    Spielername2 = prompt("Gib Spieler 2 einen Namen.")
+
+    //damit der eingegebene Spielername auf der Seite angezeigt wird.
+    document.getElementById("player1").innerText=Spielername1+" wählt...";
+    document.getElementById("player2").innerText=Spielername2+" wählt...";
 }
 
 
-function game(Auswahl){
+function selected1(Auswahl){
     Auswahl_Spieler1 = Auswahl;
-    if(Auswahl_Spieler1 === 'Schere' ){
+    if(Auswahl_Spieler1 !== undefined ){
         document.getElementById("buttons_spieler1").style="display:none;";
-        document.getElementsByClassName("Nach_Auswahl").style="display:block;";
-    }
-    
-    
+        document.getElementById("player1").innerText=Spielername1+" hat gewählt!";
+    }  
 }
+
+function selected2(Auswahl){
+    Auswahl_Spieler2 = Auswahl;
+    if(Auswahl_Spieler2 !== undefined ){
+        document.getElementById("buttons_spieler2").style="display:none;";
+        document.getElementById("player2").innerText=Spielername2+" hat gewählt!";
+    }  
+}
+
 
 
 //Funktion die üperprüft, welcher Spieler gewonnen hat. Dazu werden die Eingaben verglichen.
