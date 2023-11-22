2b1532c12049409b1cd86e8a371213e48e07254a
Miriam-MM <151748134+Miriam-MM@users.noreply.github.com>
2023-11-22 16:21:04 +0100

    Spielfunktion

--- game.js
+++ game.js
@@ -1,4 +1,16 @@
 "use strict";
+
+
+//Platzhalter für Punktzahl.
+let Anzahl_Runden
+let Spieler1_Punkte
+let Spieler2_Punkte
+let Spieler1
+let Spieler2
+let winner
+
+
+
 //Pop-up-Fenster, um beiden Spielern einen Namen zu geben.
 let Spielername1 = prompt("Gib Spieler 1 einen Namen.")
 let Spielername2 = prompt("Gib Spieler 2 einen Namen.")
@@ -13,7 +25,32 @@ function start_game(){
 }
 
 
-//Platzhalter für Punktzahl.
-let point
+function game(Auswahl){
+    Spieler1 = Auswahl;
+    if(Spieler1 === 'Schere' ){
+        document.getElementById("buttons_spieler1").style="display:hidden;"
+    }
+    
+    
+}
 
-//hallo
+
+//Funktion die üperprüft, welcher Spieler gewonnen hat. Dazu werden die Eingaben verglichen.
+//Die erste Bedingung prüft auf Unentschieden, das ist am einfachsten zu überprüfen
+//Die Zweite ob Spieler 1 Gewonnen hat
+//Treffen beide Bedingungen Nicht zu hat Spieler 2 gewonnen.
+function check_winner(eingabe1,eingabe2){
+
+    if( eingabe1 === eingabe2){
+        alert("unentschieden");
+        return;
+    }else if((eingabe1 == "Stein" && eingabe2 == "Schere" )||(eingabe1 == "Schere" && eingabe2 == "Papier" )||(eingabe1 == "Papier" && eingabe2 == "Stein")){
+        winner = Spielername1;
+        alert(winner);
+        return;    
+    }else{
+        winner = Spielername2;
+        alert(winner);
+        return;
+    }
+}
